cef141124fb02a7c6799772211ace8ee61df51e8
JamesOlaleye <olanipekunjamesolaleye@gmail.com>
2023-12-06 22:43:12 +0100

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,6 @@
 import { Link } from 'react-router-dom';
 import mockdata from '../../db.json';
 
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import ProductList from '../components/ProductList';
-
-// const Home = () => {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch products from your mock API here (e.g., using fetch or axios)
-//     // Update the products state with the fetched data
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Product List</h1>
-//       <ProductList products={products} />
-//     </div>
-//   );
-// };
-
-// export default Home;
 
 export default function Products(){
     const products = mockdata.products;
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -2,36 +2,11 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import mockdata from '../../db.json';
 
-// const ProductDetailPage = () => {
-//   const { id } = useParams();
-//   const [product, setProduct] = useState(null);
-
-//   useEffect(() => {
-//     // Fetch product details based on the ID from your mock API
-//     // Update the product state with the fetched data
-//   }, [id]);
-
-//   if (!product) {
-//     return <p>Loading...</p>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>{product.name}</h1>
-//       <img src={product.image} alt={product.name} />
-//       <p>{product.price}</p>
-//       {/* Add more details here (e.g., description, category) */}
-//     </div>
-//   );
-// };
-
-// export default ProductDetailPage;
 
 export default function ProductDetailPage() {
     const { id } = useParams();
     const productId = Number(id);
-    // const product = mockdata.products.find(product => product.id === productId);
-    // console.log(product);
+    
     const [product, setProduct] = useState({
         id: 0,
         name: '',
